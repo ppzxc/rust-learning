b7f730542bef3728d33ae01962134402fe24befc
root <root@DESKTOP-P7AMEMK.localdomain>
2021-02-24 11:28:35 +0900

    2021-02-24

---
+++ aggregator/src/lib.rs
@@ -0,0 +1,40 @@
+// trait
+pub trait Summarizable {
+    fn summary(&self) -> String;
+}
+
+// NewArticle
+pub struct NewsArticle {
+    pub headline: String,
+    pub location: String,
+    pub author: String,
+    pub content: String,
+}
+
+impl Summarizable for NewsArticle {
+    fn summary(&self) -> String {
+        format!("{}, by {} ({})", self.headline, self.author, self.location)
+    }
+}
+
+pub struct Tweet {
+    pub username: String,
+    pub content: String,
+    pub reply: bool,
+    pub retweet: bool,
+}
+
+impl Summarizable for Tweet {
+    fn summary(&self) -> String {
+        format!("{}: {}", self.username, self.content)
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
---
+++ src/hashmaps.rs
@@ -0,0 +1,7 @@
+use std::collections::HashMap;
+
+pub fn hashmaps() {
+    let mut scores = HashMap::new();
+    scores.insert(String::from("Blue"), 10);
+    scores.insert(String::from("Yellow"), 50);
+}
