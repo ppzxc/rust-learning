5f82575dbb1cbdf4d5853ca4d0546a61eb4f75a0
JeongHa-Cho <cjh8487@naver.com>
2021-04-07 14:08:45 +0900

    2021-04-07 reset

--- src/hashmaps.rs
+++
@@ -1,7 +0,0 @@
-use std::collections::HashMap;
-
-pub fn hashmaps() {
-    let mut scores = HashMap::new();
-    scores.insert(String::from("Blue"), 10);
-    scores.insert(String::from("Yellow"), 50);
-}
